b24e1e782a921b313078a81e21b4f449cb47b356
bpoile <bpoile@eecs.wsu.esu>
2020-02-23 02:19:31 -0800

    Slight bug fix -- SignUpForm class didn't match HTML in signup

--- app/forms/auth.py
+++ app/forms/auth.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, Email, ValidationError
+from wtforms.validators import DataRequired, Length, ValidationError
 from app.models.user import User
 
 class SignupForm(FlaskForm):
